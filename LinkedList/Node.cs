c22a979ab826595a32cfaa9825f94f763ad1b099
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-09-04 09:35:10 +0300

    מחלקה Node גנרית

--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -1,33 +1,39 @@
 namespace data_structures.LinkedList;
 
-public class Node
+public class Node<T>
 {
-    private Node _next;
+    private T _value;
+    private Node<T> _next;
+    
+    public T GetValue() => _value;
 
-    public Node Next
+    public void SetValue(T value)
     {
-        get { return this._next; }   
-        set { this._next = Next; }  
+        _value = value;
+    }
+    public Node<T> GetNext() => _next;
+
+    public void SetNext(Node<T> next)
+    {
+        _next = next;
     }
 
-    private int _value;
-    public int Value
+    public Node(T value)
     {
-        get
-        {
-            return this._value;
-        }
-        set
-        {
-            this._value = Value;
-        }
-
-    }
-
-    public Node(int value)
-    {
-        this._next = null;
         this._value = value;
+        this._next = null;
+    }
+
+    public Node(T value, Node<T> next)
+    {
+        this._value = value;
+        this._next = next;
+    }
+    public bool HasNext() => _next != null;
+
+    public override string ToString()
+    {
+        return _value.ToString();
     }
     
     
