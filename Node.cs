d2c6091a210bed7be6a8baaba0c6f6d24cf30c5f
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-09-04 13:36:37 +0300

    מחלקת node מתוקנת וגנרית

--- Node.cs
+++ Node.cs
@@ -1,9 +1,10 @@
-namespace data_structures.LinkedList;
+namespace data_structures;
 
 public class Node<T>
 {
     private T _value;
     private Node<T> _next;
+    private Node<T> _previous;
     
     public T GetValue() => _value;
 
@@ -17,19 +18,28 @@ public class Node<T>
     {
         _next = next;
     }
+    public Node<T> GetPrevious() => _previous;
+
+    public void SetPrevious(Node<T> previous)
+    {
+        _previous = previous;
+    }
 
     public Node(T value)
     {
         this._value = value;
         this._next = null;
+        this._previous = null;
     }
 
-    public Node(T value, Node<T> next)
+    public Node(T value, Node<T> next , Node<T> previous)
     {
         this._value = value;
         this._next = next;
+        this._previous = previous;
     }
     public bool HasNext() => _next != null;
+    public bool HasPrevious() => _previous != null;
 
     public override string ToString()
     {
